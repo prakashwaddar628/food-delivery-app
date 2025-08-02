5b56737670d30e87e6f6fee6d3741c028aa881b4
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-08-02 18:29:47 +0530

    env data added

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,7 @@
-import { Platform } from "react-native";
-
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  Platform: "com.pw.foodordering",
+  Platform: process.env.PLATFORM,
+  databaseId: process.env.DATABASE_ID,
+  usersCollections: process.env.USER_COLLECTIONS,
 };
