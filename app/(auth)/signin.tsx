5469ac7e1301317aae0040a74e30319ff378ff11
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-08-08 20:09:25 +0530

    implementing signin

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,22 +1,27 @@
-import CustomButton from '@/components/CustomButton';
-import CustomInput from '@/components/CustomInput';
-import { router } from 'expo-router';
-import { View, Text, Button } from 'react-native';
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { router } from "expo-router";
+import { View, Text, Button } from "react-native";
 
 export default function SignIn() {
   return (
-    <View className='gap-10 bg-white rounded-lg p-5 mt-5'>
-      <Text>sign-in</Text>
-      <Button title='Sign In' onPress={() => router.push('/signup')} />
-
-        <CustomInput
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
         placeholder="enter your username (@email.com)"
         label="Username"
         keyboardType="email-address"
         value=""
         onChangeText={(text) => {}}
-        />
-        <CustomButton title="Sign In" onPress={() => {}} />
+      />
+
+      <CustomInput
+        placeholder="enter your password"
+        label="Password"
+        secureTextEntry={true}
+        value=""
+        onChangeText={(text) => {}}
+      />
+      <CustomButton title="Sign In" onPress={() => {}} />
     </View>
-  )
+  );
 }
