15dceff78ac7d0a55e1d31eb2ab115416ef54d8d
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-08-08 21:52:03 +0530

    sign in logic added

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,27 +1,57 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
-import { router } from "expo-router";
-import { View, Text, Button } from "react-native";
+import { router, Link } from "expo-router";
+import { useState } from "react";
+import { View, Text } from "react-native";
 
 export default function SignIn() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
+  
+  const submit = async () =>{
+    if (!form.email || !form.password) {
+      alert("Please fill in all fields");
+      return;
+    }
+    setIsSubmitting(true);
+
+    try{
+      // call appwrite sign in function
+      alert("Sign in successful");
+      router.replace("/");
+    }catch(error){
+      alert("Sign in failed");
+    }finally{
+      setIsSubmitting(false);
+    }
+  }
+
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
       <CustomInput
         placeholder="enter your username (@email.com)"
         label="Username"
         keyboardType="email-address"
-        value=""
-        onChangeText={(text) => {}}
+        value={form.email}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
       />
 
       <CustomInput
         placeholder="enter your password"
         label="Password"
         secureTextEntry={true}
-        value=""
-        onChangeText={(text) => {}}
+        value={form.password}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, password: text }))}
       />
-      <CustomButton title="Sign In" onPress={() => {}} />
+      <CustomButton isLoading={isSubmitting} title="Sign In" onPress={() => submit()} />
+      <View className="flex justify-center flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Don't have an account?{" "}
+          <Link className="base-bold text-primary" href="/signup">
+            Sign Up
+          </Link>
+        </Text>
+      </View>
     </View>
   );
 }
