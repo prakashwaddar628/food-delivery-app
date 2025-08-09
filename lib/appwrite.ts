a92f8edec58bd04b01bb26f296f11be5b48b78c8
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-08-09 11:49:00 +0530

    appwrite configured for signup

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,64 @@
+import { CreateUserPrams, SignInParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  Platform: process.env.PLATFORM,
-  databaseId: process.env.DATABASE_ID,
-  usersCollections: process.env.USER_COLLECTIONS,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+  platform: process.env.PLATFORM!,
+  databaseId: process.env.DATABASE_ID!,
+  usersCollections: process.env.USER_COLLECTIONS!,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+const avatars = new Avatars(client);
+
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserPrams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name);
+    if (!newAccount) {
+      throw new Error("User creation failed");
+    }
+    await signIn({
+      email,
+      password,
+    });
+
+    const avatarUrl = avatars.getInitialsURL(name);
+
+    return await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollections,
+      ID.unique(),
+      { email, name, accountId: newAccount.$id, avatar: avatarUrl }
+    );
+
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+};
+
+export const signIn = async ({ email, password }: SignInParams) => {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+    if (!session) {
+      throw new Error("Sign in failed");
+    }
+    return session;
+  } catch (error) {
+    console.error("Error signing in:", error);
+    throw error;
+  }
 };
